b0693e66286f38cc297c5564bf80c225b6474c19
Tim Macdonald <macdonald.tim@gmail.com>
2015-10-21 22:02:35 -0400

    Got browserSync working

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+// Watch files for changes & reload
+gulp.task('serve', [], function() {
+  browserSync({
+    notify: false,
+    // Customize the Browsersync console logging prefix
+    logPrefix: 'WSK',
+    // Allow scroll syncing across breakpoints
+    scrollElementMapping: ['main', '.mdl-layout'],
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: ['.tmp', 'app']
+  });
+});
