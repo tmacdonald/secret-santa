6b634fec0c7301f4cef1a3267df6929e0e8b82fb
Tim Macdonald <macdonald.tim@gmail.com>
2015-10-21 22:43:51 -0400

    Added list

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,14 @@
 var app = angular.module('SecretSanta', ['ngMaterial']);
 
 var AppController = app.controller('AppCtrl', ['$scope', function($scope) {
+  var vm = this;
 
+  vm.people = [{
+    name: 'Tim',
+    newMessage: true
+  }, {
+    name: 'Remi',
+  }, {
+    name: 'Sicco'
+  }];
 }]);
