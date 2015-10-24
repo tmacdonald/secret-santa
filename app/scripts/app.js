ba19467f931e6269f7e86404ee1e9363520f5b06
Tim Macdonald <macdonald.tim@gmail.com>
2015-10-23 21:02:14 -0400

    Adding and removing people from localStorage

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,31 @@
-var app = angular.module('SecretSanta', []);
+var app = angular.module('SecretSanta', ['LocalStorageModule']);
 
-var AppController = app.controller('AppCtrl', ['$scope', function($scope) {
+app.config(['localStorageServiceProvider', function(localStorageServiceProvider) {
+  localStorageServiceProvider.setPrefix('secret-santa');
+  localStorageServiceProvider.setStorageType('localStorage');
+}]);
+
+var AppController = app.controller('AppCtrl', ['localStorageService', function(localStorageService) {
   var vm = this;
+
+  vm.people = localStorageService.get('people') || [];
 
   vm.person = {
     name: ''
   };
 
   vm.addPerson = function() {
+
     vm.people.push(vm.person);
+    localStorageService.set('people', vm.people);
+
     vm.person = {
       name: ''
     };
-  }
+  };
 
-  vm.people = [];
+  vm.removePerson = function(index) {
+    vm.people.splice(index, 1);
+    localStorageService.set('people', vm.people);
+  }
 }]);
