9afbcd06eeda4cc34a479c13bf2d43fe13dcc88b
Tim Macdonald <macdonald.tim@gmail.com>
2017-09-30 23:14:34 -0400

    Added event reducers

--- react-version/src/reducers/index.js
+++ react-version/src/reducers/index.js
@@ -2,7 +2,7 @@ let memberCount = 0;
 let groupCount = 0;
 
 function addMemberToGroup(state, group, member) {
-    const i = (state.groups.indexOf(group));
+    const i = (state.groups.indexOf(group))
 
     const newGroup = {
         ...group,
@@ -10,17 +10,41 @@ function addMemberToGroup(state, group, member) {
     }
     return {
         ...state,
-        groups: [...state.groups.slice(0, i), newGroup, ...state.groups.slice(i+1)]
+        groups: [...state.groups.slice(0, i), newGroup, ...state.groups.slice(i + 1)]
     }
 }
 
 function addGroup(state, group) {
-    const id = groupCount++;
+    const id = groupCount++
     const groups = [...state.groups || [], { id, members: []}]
 
     return {
         ...state,
         groups
+    }
+}
+
+function addEvent(state, event) {
+    const id = eventCount++
+    const events = [...state.events || [], { id, matching_results: []}]
+
+    return {
+        ...state,
+        events
+    }
+}
+
+function addMatchToEvent(state, event, match) {
+    const { events } = state
+    const i = (events.indexOf(event))
+
+    const newEvent = {
+        ...event,
+        matching_results: [...event.matching_results, match]
+    }
+    return {
+        ...state,
+        events: [...events.slice(0, i), newEvent, ...events.slice(i + 1)]
     }
 }
 
@@ -30,6 +54,10 @@ function reducer(state, action) {
             return addGroup(state, action.group)
         case 'ADD_MEMBER_TO_GROUP':
             return addMemberToGroup(state, action.group, action.member)
+        case 'ADD_EVENT':
+            return addEvent(state, action.event)
+        case 'ADD_MATCH_TO_EVENT':
+            return addMatchToEvent(state, action.event, action.match)
         default:
             return state
     }
